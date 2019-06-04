c653320fd67721ae8d3fb69fbc8e6b029daefd03
JD <jdhouse27@gmail.com>
2019-06-03 21:18:15 -0500

    added JawsDB_url

--- config/connection.js
+++ config/connection.js
@@ -4,21 +4,27 @@ const mysql = require("mysql");
 const keys = require("../keys");
 const burger = keys.burger;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: burger.password,
-  database: "burgers_db"
-});
+const connection;
 
+if (process.env.JAWSDB_UL) {
+    connection = mysql.createConnection(process.env.JAWSDB_UL);
+} else {
+    connection = mysql.createConnection({
+
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: burger.password,
+        database: "burgers_db"
+    });
+};
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
